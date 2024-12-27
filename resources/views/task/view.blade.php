4c657c44acfb97eeb3bf07bbf56997aab8c70f99
nahiankhondokar <nahiankhondokarphp@gmail.com>
2024-12-27 21:29:31 +0600

    remove unnecessery code

--- resources/views/task/view.blade.php
+++ resources/views/task/view.blade.php
@@ -5,37 +5,6 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center ">
-                
-                
-                {{-- <table class="table table-striped border">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Task</th>
-                        <th scope="col">Start Date</th>
-                        <th scope="col">Eend Date</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">{{$key + 1}}</th>
-                        <td>{{$task->task}}</td>
-                        <td>{{$token->amount ?? "None"}}</td>
-                        <td>{{$token->start_date ?? "None"}}</td>
-                        <td>{{$token->end_date ?? "None"}}</td>
-                        <td>
-                            <a href="{{route('task.edit',$token->id)}}" class="text-info">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                            </a>
-                            <a href="{{route('task.delete',$token->id)}}" class="text-danger">
-                                <i class="fa-solid fa-trash"></i>
-                            </a>
-                        </td>
-                        </tr>
-                    </tbody>
-                </table> --}}
 
                 <div class="task-view shadow rounded">
                     <h3 class="text-center">View Task</h3>
@@ -53,7 +22,7 @@
 
                     <div class="task-item">
                         <span>Status :</span>
-                        <h5><div class="badge badge-primary">{{$task->status}}</div></h5>
+                        <h5><div class="badge">{{$task->status}}</div></h5>
                     </div>
 
                     <div class="task-item">
